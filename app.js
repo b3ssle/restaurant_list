f809110e154a14fb520b9d6f56661b5a2c9e153e
b3ssle <hi@b3ssle.be>
2022-07-02 22:08:52 +0900

    feat: add create restaurant  function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const exphbs = require("express-handlebars")
+const bodyParser = require('body-parser')
 const restaurantsData = require("./restaurant.json").results
+const Restaurant = require("./models/Restaurant")
 
 const app = express()
 const port = 3000
@@ -9,22 +11,26 @@ const port = 3000
 require('dotenv').config()
 
 const url = process.env.MONGO_URL
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost/restaurant-list", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log("Server up and running!")))
-    .catch((error) => console.log(error.message)) 
+  .catch((error) => console.log(error.message))
 mongoose.set('useFindAndModify', false)
 
 const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
+
+db.on("error", () => {
+  console.log("mongodb error!")
 })
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
+db.once("open", () => {
+  console.log("mongodb connected!")
+})
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
+app.set("view engine", "hbs")
 app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   res.render("index", { restaurantsData })
@@ -47,12 +53,25 @@ app.get("/search", (req, res) => {
   res.render("index", { restaurantsData: filterRestaurantsData, keywords })
 })
 
+// 新增餐廳頁面
+app.get("/restaurants/new", (req, res) => {
+  res.render("new")
+})
+
+// 瀏覽特定餐廳
 app.get("/restaurants/:restaurantId", (req, res) => {
   const { restaurantId } = req.params
   const restaurantData = restaurantsData.find(
     data => data.id === Number(restaurantId)
   )
   res.render("show", { restaurantData })
+})
+
+// 新增餐廳
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
